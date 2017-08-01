8b18fa0076dc8b9ed0384d270445b0cdc9607fc6
Jennifer Harkreader <jen3072@gmail.com>
2017-07-31 19:22:41 -0700

    printJobs: added logic for no results, added placeholder test for job data

--- src/org/launchcode/techjobs/console/TechJobs.java
+++ src/org/launchcode/techjobs/console/TechJobs.java
@@ -111,6 +111,18 @@ public class TechJobs {
     // Print a list of jobs
     private static void printJobs(ArrayList<HashMap<String, String>> someJobs) {
 
-        System.out.println("printJobs is not implemented yet");
+        if (someJobs.size() == 0) {
+            System.out.println("No jobs found with that criteria.");
+        }
+        else {
+            System.out.println("Jobs found: \n");
+            for (int i=0; i<someJobs.size(); i++) {
+                System.out.println("****");
+                System.out.println("Job data here");
+                System.out.println("****\n");
+            }
+        }
+
+
     }
 }
